1822a360e9bee74a694269f0cb876dc486a208fc
KiranPila-Shrestha <np01cp4a210178@islingtoncollege.edu.np>
2024-01-07 11:15:29 +0545

    Project set up done.

---
+++ BileriumCafe_POS/BileriumCafe_POS/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BileriumCafe_POS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BileriumCafe_POS/BileriumCafe_POS/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BileriumCafe_POS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
